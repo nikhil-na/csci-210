4b79ba77c2ae12517c56c381b3d2f3354724abcd
Nikhil Aryal <aryaln0058@gmail.com>
2023-11-02 22:01:00 -0500

    Designed landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
+import LandingPage from './components/LandingPage';
 import Login from '../src/components/user/login';
 import Signup from '../src/components/user/signup';
 
@@ -9,10 +9,11 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path = '/' element={<LandingPage />} />
         <Route path = '/login' element={<Login />} />
         <Route path = '/signup' element={<Signup />} />
       </Routes>
     </BrowserRouter>
-)}
-
+  );
+}
 export default App;
